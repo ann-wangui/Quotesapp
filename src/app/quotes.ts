448f791b052eada3ac1c1efd53c6ded1647d1b28
ann-wangui <wanguia822@gmail.com>
2022-04-27 15:23:50 +0300

    moved quotes files

---
+++ src/app/quotes.ts
@@ -0,0 +1,26 @@
+export class Quotes {
+    showDescription: boolean;
+    upvotes: number;
+    downvotes: number;
+    completeDate: Date; 
+    
+  
+    constructor( showDescription:boolean, upvotes:number, downvotes:number, completeDate:Date){
+    
+      this.showDescription=false;
+      this.upvotes =0;
+      this.downvotes =0;
+      this.completeDate=new Date;
+    }
+  }// export class Quotes {
+  //     id: number;
+  //     quote: string;
+  //     upvotes: number;
+  //     downvotes: number;
+  //     constructor(id:number, quote:string, upvotes:number, downvotes:number){
+  //       this.id = id;
+  //       this.quote = quote;
+  //       this.upvotes = upvotes;
+  //       this.downvotes = downvotes;
+  //     }
+  // }
